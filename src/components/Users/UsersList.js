9055acd6c1c6d15d09f5dda30a58483ca673d085
1215380 <abhijeet.pathare@infosys.com>
2022-08-24 23:49:47 +0530

    add update

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const UsersList = (props) => {
   return (
-    <div >
+    <div className='container'>
       <ul>
         {props.users.map((user) => { 
           return (
             
             <li key={user.id}>{user.name} ({user.age} years old) </li>
             
+
           );
         })}
       </ul>
